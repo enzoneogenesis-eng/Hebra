7ec3afa116e1184d55f55d75f3ea0ab627df1947
enzoneogenesis-eng <enzo.neogenesis@gmail.com>
2026-04-17 10:08:22 -0300

    Fix imagenes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,2 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [{
-      protocol: "https",
-      hostname: "*.supabase.co",
-    }],
-  },
-};
-export default nextConfig;
+const nextConfig = { images: { remotePatterns: [ { protocol: "https", hostname: "images.unsplash.com" }, { protocol: "https", hostname: "ui-avatars.com" }, { protocol: "https", hostname: "*.supabase.co" } ] } }; 
+export default nextConfig; 
